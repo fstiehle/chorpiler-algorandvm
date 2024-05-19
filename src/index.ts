dafb4195ae6258be745849929da686c96b215aa7
Fabian <f.stiehle@gmail.com>
2024-05-19 15:15:38 +0200

    package ready

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { TealContractGenerator } from "./Generator/target/Teal/TealContractGenerator";
+
+export default {
+  generators: {
+    teal: {
+      DefaultContractGenerator: TealContractGenerator
+    }
+  }
+}
